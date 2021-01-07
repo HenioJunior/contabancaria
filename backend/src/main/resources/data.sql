cc3bf531b41a97921f08dc11cafd52d7bcf47940
Henio Junior <henioajunior@gmail.com>
2021-01-07 14:42:22 -0300

    Domain model

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_cliente (nome, email, cpf, data_nascimento) VALUES ('James T. Kirk', 'jameskirk@gmail.com', '01225898752', '1948-03-22');
+INSERT INTO tb_cliente (nome, email, cpf, data_nascimento) VALUES ('Leonard McCoy', 'leonard@gmail.com', '01235496752', '1937-06-01');
+INSERT INTO tb_cliente (nome, email, cpf, data_nascimento) VALUES ('Alex Green', 'alex@gmail.com', '05878565233', '1963-01-10');
+INSERT INTO tb_cliente (nome, email, cpf, data_nascimento) VALUES ('Bob Lazar', 'bob@gmail.com', '03429899752', '1965-03-07');
+INSERT INTO tb_cliente (nome, email, cpf, data_nascimento) VALUES ('Maria Lima', 'maria@gmail.com', '05425868752', '1988-01-22');
+
